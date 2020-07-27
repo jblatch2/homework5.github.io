0706317071ed8af34140d3a26e6da32f91989d2a
jblatch2 <66186725+jblatch2@users.noreply.github.com>
2020-07-26 21:20:13 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// date and time in header
+setInterval(() => {
+  moment(Date);
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm:ss a'));
+
+}, 1000)
+
+// for loop, changing color to each row
+  for(i = 9; i < 18; i++) {
+    // variable for current hour
+    var currentHour = parseInt(moment().format('HH')); 
+    // variable for the row ID
+    var rowHour = $("#hour-" + i);
+    // if it's the currrent hour, the row will be red
+    if (currentHour === i) {
+      $("#hour-" + i).addClass("present");
+    }
+    // if it's a past hour, the row will be grey
+    if (currentHour > i) {
+      $("#hour-" + i).addClass("past");
+    }
+    //if the hour is in the future, the row will be green
+    if (currentHour < i) {
+      $("#hour-" + i).addClass("future");
+    }
+    //pulls data from local storage to view in textarea
+    $("#hour-" + i).val(localStorage.getItem("hour-" + i));
+
+  }
+
+// click function to save textarea data to local storage
+
+$(".saveBtn").on("click", function () {
+    var id = $(this).attr("data-id");
+    var value = $("#" + id).val();
+    localStorage.setItem(id, value);
+  });
